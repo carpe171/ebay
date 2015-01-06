a9a94930b98d46cb178941ea75c8e0cb9a18622b
Homan Chou <homanchou@gmail.com>
2015-01-06 14:58:06 -0800

    make all user preference inputs optional

--- lib/ebay/requests/get_user_preferences.rb
+++ lib/ebay/requests/get_user_preferences.rb
@@ -23,9 +23,9 @@ module Ebay # :nodoc:
       include XML::Mapping
       include Initializer
       root_element_name 'GetUserPreferencesRequest'
-      boolean_node :show_bidder_notice_preferences, 'ShowBidderNoticePreferences', 'true', 'false'
-      boolean_node :show_combined_payment_preferences, 'ShowCombinedPaymentPreferences', 'true', 'false'
-      boolean_node :show_cross_promotion_preferences, 'ShowCrossPromotionPreferences', 'true', 'false'
+      boolean_node :show_bidder_notice_preferences, 'ShowBidderNoticePreferences', 'true', 'false', :optional => true
+      boolean_node :show_combined_payment_preferences, 'ShowCombinedPaymentPreferences', 'true', 'false', :optional => true
+      boolean_node :show_cross_promotion_preferences, 'ShowCrossPromotionPreferences', 'true', 'false', :optional => true
       boolean_node :show_dispatch_cutoff_time_preferences, 'ShowDispatchCutoffTimePreferences', 'true', 'false', :optional => true
       boolean_node :show_email_shipment_tracking_number_preference, 'ShowEmailShipmentTrackingNumberPreference', 'true', 'false', :optional => true
       boolean_node :show_end_of_auction_email_preferences, 'ShowEndOfAuctionEmailPreferences', 'true', 'false', :optional => true
@@ -39,8 +39,8 @@ module Ebay # :nodoc:
       boolean_node :show_required_ship_phone_number_preference, 'ShowRequiredShipPhoneNumberPreference', 'true', 'false', :optional => true
       boolean_node :show_seller_exclude_ship_to_location_preference, 'ShowSellerExcludeShipToLocationPreference', 'true', 'false', :optional => true
       boolean_node :show_seller_favorite_item_preferences, 'ShowSellerFavoriteItemPreferences', 'true', 'false', :optional => true
-      boolean_node :show_seller_payment_preferences, 'ShowSellerPaymentPreferences', 'true', 'false'
-      boolean_node :show_seller_profile_preferences, 'ShowSellerProfilePreferences', 'true', 'false'
+      boolean_node :show_seller_payment_preferences, 'ShowSellerPaymentPreferences', 'true', 'false', :optional => true
+      boolean_node :show_seller_profile_preferences, 'ShowSellerProfilePreferences', 'true', 'false', :optional => true
       boolean_node :show_seller_return_preferences, 'ShowSellerReturnPreferences', 'true', 'false', :optional => true
       boolean_node :show_unpaid_item_assistance_exclusion_list, 'ShowUnpaidItemAssistanceExclusionList', 'true', 'false', :optional => true
       boolean_node :show_unpaid_item_assistance_preference, 'ShowUnpaidItemAssistancePreference', 'true', 'false', :optional => true
