f728b6a1506d2bdf09658299ce948e4a389028c9
Homan Chou <homanchou@gmail.com>
2015-03-19 11:00:07 -0700

    xsd missing minOccurs=0 for some unrequired elements, port over some fixes made by jay

--- lib/ebay/types/revise_status.rb
+++ lib/ebay/types/revise_status.rb
@@ -2,7 +2,7 @@
 module Ebay # :nodoc:
   module Types # :nodoc:
     # == Attributes
-    #  boolean_node :item_revised, 'ItemRevised', 'true', 'false'
+    #  boolean_node :item_revised, 'ItemRevised', 'true', 'false', :optional => true
     #  boolean_node :buy_it_now_added, 'BuyItNowAdded', 'true', 'false', :optional => true
     #  boolean_node :buy_it_now_lowered, 'BuyItNowLowered', 'true', 'false', :optional => true
     #  boolean_node :reserve_lowered, 'ReserveLowered', 'true', 'false', :optional => true
@@ -11,7 +11,7 @@ module Ebay # :nodoc:
       include XML::Mapping
       include Initializer
       root_element_name 'ReviseStatus'
-      boolean_node :item_revised, 'ItemRevised', 'true', 'false'
+      boolean_node :item_revised, 'ItemRevised', 'true', 'false', :optional => true
       boolean_node :buy_it_now_added, 'BuyItNowAdded', 'true', 'false', :optional => true
       boolean_node :buy_it_now_lowered, 'BuyItNowLowered', 'true', 'false', :optional => true
       boolean_node :reserve_lowered, 'ReserveLowered', 'true', 'false', :optional => true
